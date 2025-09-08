e1bf56b53fd118f154553efc0bc67029f3c5fff0
Иван <duryagin1995@yandex.ru>
2025-09-08 10:58:24 +0300

    7th iteration

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,28 @@
 package actioninfo
 
+import (
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, v := range dataset {
+		err := dp.Parse(v)
+		if err != nil {
+			log.Printf("Data parsing error: %v", err)
+			continue
+		}
+
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Printf("Error receiving training information: %v", err)
+			continue
+		}
+
+		log.Println(info)
+	}
 }
